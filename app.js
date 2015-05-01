c4635442429a47091ab45add8223499990efd99a
dsesto <d.sesto.94@gmail.com>
2015-05-01 18:05:46 +0200

    CORS

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
-var express  = require("express"),
-    app      = express(),
-    http     = require("http"),
-    server   = http.createServer(app),
+var express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = http.createServer(app),
+    cors = require('cors'),
     mongoose = require('mongoose');
+
 
 app.configure(function () {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
+    app.use(cors());
     app.use(app.router);
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.send("Hello world!");
 });
 
@@ -19,14 +22,14 @@ routes = require('./routes/events')(app);
 routes = require('./routes/messages')(app);
 
 
-mongoose.connect('mongodb://localhost/TravelShare', function(err, res) {
-    if(err) {
+mongoose.connect('mongodb://localhost/TravelShare', function (err, res) {
+    if (err) {
         console.log('ERROR: connecting to Database. ' + err);
     } else {
         console.log('Connected to Database');
     }
 });
 
-server.listen(3000, function() {
+server.listen(3000, function () {
     console.log("Node server running on http://localhost:3000");
 });
