d64f3f0de756099c9416c26849853896623006c6
yifei ge <yokihappy@hotmail.com>
2015-05-07 10:21:22 +0200

    Creado el login del web

--- Web/Login_Angular/app/app.js
+++ Web/Login_Angular/app/app.js
@@ -1,17 +1,67 @@
-'use strict';
-
 // Declare app level module which depends on views, and components
 (function (){
 
-var app=angular.module('loginApp', []);
+    var obj={};
+    var box={};
+    var app=angular.module('loginApp', []);
 
-app.controller('loginController',['$http','$log',function($http,$log)
-{
-    $http.get('http://localhost:3000/users').success(function(data)
+    app.controller('loginController',['$http','$log','$scope','$window',function($http,$log,$scope,$window)
     {
-        $log.debug('hemos hecho la petition get al API');
-        $log.debug(data[3].username);
-        $log.debug(data[3].password);
-    });
-}]);
+        $scope.userInfo={};
+        box=$scope.userInfo;
+        $log.debug(box);
+        $scope.login=function()
+        {
+
+            var res= $http.post('http://localhost:3000/login',box);
+            res.success(function(data)
+            {
+                if(data.username!=null)
+                {
+                    alert("Username and password match");
+                    $window.location.href='http://localhost:63342/Travel-Share/Web/Victor/home.html';
+                    $scope.userInfo.username='';
+                    $scope.userInfo.password='';
+                }
+                else
+                {
+                    alert("Username or password invalid");
+                    $scope.userInfo.username='';
+                    $scope.userInfo.password='';
+
+                }
+
+            });
+            res.error(function(error)
+            {
+                alert("An error has occured");
+            });
+
+        };
+
+
+
+
+
+
+    }]);
+    /*app.controller('userController',['$scope','$log',function($scope,$log)
+     {
+     $scope.userInfo={};
+     box=$scope.userInfo;
+     $log.debug(box);
+
+     $scope.login=function()
+     {
+     if(obj[3].username===box.name && obj[3].password===box.pass)
+     {
+     alert("Username and password match");
+     }
+     else
+     {
+     alert("Username or password is invalid");
+     }
+     };
+
+     }]);*/
 })();
