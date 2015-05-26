8a02dd7e607c3369ab784d8372aa8e12c0bc15cf
vmiranda7 <v_miranda7@hotmail.com>
2015-05-26 12:56:16 +0200

    comit2sprint

--- www/js/home.js
+++ www/js/home.js
@@ -4,8 +4,13 @@
 var map;
 var marcadores=[];
 var markers=[];
+//var marker=[];
+var new_marker;
 var filteredmarkers=[];
 var geolocation;
+var new_event_click= false;
+var usenrame= "prueba";
+var username_id="556324cff5dc436409000001";
 
 var tags = [{
     nombre: "sin filtro",
@@ -13,39 +18,62 @@ var tags = [{
     color : "white"
 },{
     nombre: "Deporte",
+    tag: "Deporte",
     id: 1,
     color : "lightgreen"
-}, {
+},{
     nombre: "Música",
+    tag: "Musica",
     id: 2,
     color: "lightskyblue"
 },{
     nombre: "Cultura",
+    tag: "Cultura",
     id: 3,
     color : "yellow"
 },{
     nombre: "Fiesta",
+    tag: "Fiesta",
     id: 4,
     color: "palevioletred"
 }];
 
+
+
+function newevent(){
+    document.getElementById("panel_tag").style.visibility = 'hidden';
+    document.getElementById("panel_info").style.visibility = 'hidden';
+    document.getElementById("panel_new_event").style.visibility = 'visible';
+    new_event_click=true;
+}
+
+function exitpanel(){
+    document.getElementById("panel_tag").style.visibility = 'visible';
+    document.getElementById("panel_info").style.visibility = 'hidden';
+    document.getElementById("panel_new_event").style.visibility = 'hidden';
+    //new_event_click=false;
+    setAllMap(map);
+    new_marker = "";
+
+}
+
 //Carga los marcadores de la API
 (function() {
     var app = angular.module('Eventos', []);
-    app.controller('CargarEventos', ['$http', '$log', function ($http, $log) {
+    app.controller('CargarEventos', ['$http', '$log', function ($http) {
+
         $http.get('http://localhost:3000/events').success(function (data) {
             console.log(data);
             marcadores = data;
-           /* for (var i = 0; i < data.length; i++) {
-                var object = data[i];
-
-            }*/
         });
     }]);
 })();
 
 
+
 function initialize() {
+
+
 
     var mapOptions = {
         zoom: 12,
@@ -80,6 +108,7 @@ function initialize() {
     google.maps.event.addListener(map, 'click', function(e)
     {
         placeMarker(e.latLng, map);
+
     });
 
     // se añanden los macadores y se añade sus valores
@@ -102,20 +131,80 @@ function initialize() {
     map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     //var searchBox = new google.maps.places.SearchBox((input));
 }
-
-/*
-function placeMarker(position, map) {
-
-    var content="Crear un evento";
-    var event="undefined";
-    var detalle = "undefined"
-    var tag = 0;
-    addMarker(position,detalle,map,content,event,tag);
-    // Te centra el marcador en el centro del mapa
-}*/
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
+
+
+
+function setAllMap(map) {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(map);
+    }
+}
+
+
+function new_marker_post(){
+    var e = document.getElementById("event_tag");
+    var idtag = e.options[e.selectedIndex].value;
+
+
+    var new_event =new Object();
+    new_event.eventname = $("#new_eventname").val();
+    new_event.tag = tags[idtag].tag;
+    new_event.idtag = idtag;
+    new_event.description = $("#new_description").val();
+    new_event.owner = username_id;
+    var lat = new_marker.position.lat();
+    var lng= new_marker.position.lng();
+    var place= new Array();
+        place[0] = ''+lat+'';
+        place[1] = ''+lng+'';
+    //console.log('["'+new_marker.position.lat()+'","'+new_marker.position.lng()+'"]');
+    new_event.place = place;
+    new_event.date = $("#datetimepicker6").val();
+    console.log(place);
+    var data = JSON.stringify(new_event);
+    console.log(data);
+    $.ajax({
+            url: "http://localhost:3000/event",
+            type: 'POST',
+            crossDomain: true,
+            dataType: 'json',
+            contentType: 'application/json',
+            data: data,
+            success: function (data) {
+                console.log("correct");
+            },
+            error: function () {
+
+            }
+        });
+}
+
+function placeMarker(position, map) {
+    if(new_event_click==true)
+    {
+        //setAllMap(null);
+        new_marker= new google.maps.Marker
+        ({
+            position: position,
+            draggable: true,
+            map: map
+        });
+        pushMarker(new_marker);
+       // var pos = new_marker.position.split("(")
+        document.getElementById("label").innerText = new_marker.position.lat()+","+new_marker.position.lng();
+        map.setCenter(new_marker.getPosition());
+
+        new_event_click = false;
+
+        google.maps.event.addListener(new_marker, 'dragend', function(e) {
+            document.getElementById("label").innerText = e.latLng.lat() + ',' + e.latLng.lng();
+            map.setCenter(new_marker.getPosition());
+        });
+    }
+}
 
 
 function info_evento(marker){
