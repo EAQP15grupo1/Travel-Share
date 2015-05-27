bf016e84805ab2b031db55f636b44af968f587e7
dsesto <d.sesto.94@gmail.com>
2015-05-27 17:04:14 +0200

    Producción

--- models/event/schema.js
+++ models/event/schema.js
@@ -9,13 +9,13 @@ var mongoose = require('mongoose'),
 var schema = new Schema({
         eventname: {type: String},
         tag: {type: String, enum: ['Deporte', 'Fiesta', 'Cultura', 'Compania', 'Trabajo', 'Musica']},
-        idtag:{type: String},
+        idtag: {type: String},
         description: {type: String},
         color: {type: String},
         owner: {type: Schema.Types.ObjectId, ref: 'User'},
         attendees: [{type: Schema.Types.ObjectId, ref: 'User'}],
         place: [{type: Number}],
-        date: {type: Date}
+        date: {type: String}
     },
     {versionKey: false});
 
