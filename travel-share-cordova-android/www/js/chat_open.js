909187285b07782459d7652d00ebfe40a7f62096
dsesto <d.sesto.94@gmail.com>
2015-05-31 20:25:28 +0200

    Chat Layout

--- travel-share-cordova-android/www/js/chat_open.js
+++ travel-share-cordova-android/www/js/chat_open.js
@@ -1,6 +1,7 @@
+var chat;
+
 window.onload = function () {
     getChat();
-    $("#user").text("Tu usuario: " + window.localStorage.getItem("username"));
 }
 
 function getChat() {
@@ -13,22 +14,34 @@ function getChat() {
         crossDomain: true,
         dataType: 'json',
         success: function (data) {
-            var box = PUBNUB.$('box'), input = PUBNUB.$('input'), channel = data.chatID;
+            var box = PUBNUB.$('box'), input = $('#input'), channel = data.chatID;
+            chat = data.chatID;
+
+            if (channel.split("-")[0] == window.localStorage.getItem("username"))
+                $("#span").text("Chat con " + channel.split("-")[1]);
+            else
+                $("#span").text("Chat con " + channel.split("-")[0]);
 
             PUBNUB.subscribe({
                 channel: channel,
                 callback: function (text) {
-                    $('<br>' + '<strong>' + text.username + '</strong> <label>' + text.text + '</label>').appendTo($('#box'));
-                    //box.innerHTML = ('' + text).replace(/[<>]/g, '') + '<br>' + box.innerHTML
+                    //$('<br>' + '<strong>' + text.username + '</strong> <label>' + text.text + '</label>').appendTo($('#box'));
+
+                    if (text.username == window.localStorage.getItem("username"))
+                        $('<div class="rcorners2">' + '<label style="margin: 10px; font-size: 150%"> <strong>' + text.username + '</strong> <br> </label>' + '<label>' + text.text + '</label>' + '</div>').appendTo($('#box'));
+                    else
+                        $('<div class="rcorners1">' + '<label style="margin: 10px; font-size: 150%"> <strong>' + text.username + '</strong> <br> </label>' + '<label>' + text.text + '</label>' + '</div>').appendTo($('#box'));
+
                 }
             });
             PUBNUB.bind('keyup', input, function (e) {
                 (e.keyCode || e.charCode) === 13 && PUBNUB.publish({
                     channel: channel,
-                    message: {username: window.localStorage.getItem("username"), text: input.value},
-                    x: (input.value = '')
+                    message: {username: window.localStorage.getItem("username"), text: input.val()},
+                    x: (input.val(""))
                 })
             });
+
             PUBNUB.history({
                 channel: channel,
                 limit: 100
@@ -37,9 +50,13 @@ function getChat() {
                 messages = messages || [];
 
                 for (var i = 0; i < messages.length; i++) {
-                    console.log(messages[i]);
-                    $('<br>' + '<strong>' + messages[i].username + '</strong> <label>' + messages[i].text + '</label>').appendTo($('#box'));
-                    //box.innerHTML = ('<strong>' + messages[i].username + '</strong>' + messages[i].text).replace(/[<>]/g, '') + '<br>' + box.innerHTML;
+                    //$('<br>' + '<strong>' + messages[i].username + '</strong> <label>' + messages[i].text + '</label>').appendTo($('#box'));
+
+                    if (messages[i].username == window.localStorage.getItem("username"))
+                        $('<div class="rcorners2">' + '<label style="font-size: 150%"> <strong>' + messages[i].username + '</strong> <br> </label>' + '<label>' + messages[i].text + '</label>' + '</div>').appendTo($('#box'));
+                    else
+                        $('<div class="rcorners1">' + '<label style="margin: 10px; font-size: 150%"> <strong>' + messages[i].username + '</strong> <br> </label>' + '<label>' + messages[i].text + '</label>' + '</div>').appendTo($('#box'));
+
                 }
 
                 $(document).scrollTop($(document).height());
@@ -50,6 +67,30 @@ function getChat() {
         }
     });
 }
+
+$('#input').keyup(function () {
+    var empty = false;
+    $('#input').each(function () {
+        if ($(this).val().length == 0) {
+            empty = true;
+        }
+    });
+
+    if (empty) {
+        $('#sendButton').attr('disabled', 'disabled');
+    } else {
+        $('#sendButton').removeAttr('disabled');
+    }
+});
+
+function send() {
+    var input = $('#input');
+    PUBNUB.publish({
+        channel: chat,
+        message: {username: window.localStorage.getItem("username"), text: input.val()},
+        x: (input.val(""))
+    })
+};
 
 //(function () {
 //    var box = PUBNUB.$('box'), input = PUBNUB.$('input'), channel = 'chat';
