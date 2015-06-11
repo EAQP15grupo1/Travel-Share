30657f8b3fd3b7a0223e7c4af009416e72446fbc
dsesto <d.sesto.94@gmail.com>
2015-06-11 15:53:26 +0200

    Facebook

--- travel-share-cordova-android/www/js/login.js
+++ travel-share-cordova-android/www/js/login.js
@@ -56,29 +56,11 @@ $("#RegisterBtn").click(function () {
 });
 
 $("#FacebookBtn").click(function () {
-    //$.ajax({
-    //    url: "http://147.83.7.201:3000/facebook",
-    //    type: 'GET',
-    //    crossDomain: true,
-    //    dataType: 'json',
-    //    success: function (data) {
-    //        //window.location.href = 'updateProfile.html';
-    //        console.log(data);
-    //    },
-    //    error: function (error_API) {
-    //        console.log(error_API);
-    //        //window.alert(error_API.response);
-    //        window.localStorage.setItem("userID", error_API.userId);
-    //        window.localStorage.setItem("token", error_API.token);
-    //        //window.location.href = 'updateProfile.html';
-    //    }
-    //});
-
     $.ajax({
         url: "http://147.83.7.201:3000/facebook",
         type: 'GET',
         crossDomain: true,
-        dataType: 'jsonp',
+        dataType: 'json',
         success: function (data) {
             //window.location.href = 'updateProfile.html';
             console.log(data);
@@ -86,9 +68,47 @@ $("#FacebookBtn").click(function () {
         error: function (error_API) {
             console.log(error_API);
             //window.alert(error_API.response);
-            window.localStorage.setItem("userID", error_API.userId);
+            //window.localStorage.setItem("userID", error_API.userId);
             //window.localStorage.setItem("token", error_API.token);
             //window.location.href = 'updateProfile.html';
         }
     });
+
+    //var url = "http://localhost:3000/facebook";
+    //var method = "GET";
+    //
+    //var xhr = new XMLHttpRequest();
+    //
+    //if ("withCredentials" in xhr) {
+    //    // Most browsers.
+    //    xhr.open(method, url, true);
+    //    xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+    //    xhr.setRequestHeader("Access-Control-Allow-Credentials", "true");
+    //    xhr.setRequestHeader("Access-Control-Allow-Methods", "GET");
+    //}
+    //else if (typeof XDomainRequest != "undefined") {
+    //    // IE8 & IE9
+    //    xhr = new XDomainRequest();
+    //    xhr.open(method, url);
+    //}
+    //else {
+    //    // CORS not supported.
+    //    xhr = null;
+    //}
+    //
+    //if (xhr != null) {
+    //    xhr.followsRedirect = false;
+    //    xhr.withCredentials = true;
+    //    xhr.onreadystatechange = function () {
+    //        if (xhr.readyState == 4) {
+    //            if (xhr.status == 200) {
+    //                alert(xhr.responseText);
+    //            }
+    //            else {
+    //                alert("error");
+    //            }
+    //        }
+    //    }
+    //    xhr.send();
+    //}
 });
