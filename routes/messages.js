76fc92e1f31c42d43af7c73ad85b37ec3a9bca30
dsesto <d.sesto.94@gmail.com>
2015-06-12 20:12:31 +0200

    Backoffice

--- routes/messages.js
+++ routes/messages.js
@@ -83,6 +83,7 @@ module.exports = function (app) {
     //UPDATE Message
     updateMessage = function (req, res) {
         console.log('UPDATE message');
+        console.log(req.params._id);
         Message.findOneAndUpdate({"_id": req.params._id}, req.body, function (err, data) {
             console.log(data);
             //console.log(data._id);
