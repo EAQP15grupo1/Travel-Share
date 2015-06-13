47f3cc36d9e052a3d620e3cb6b2ba20e9db8b940
dsesto <d.sesto.94@gmail.com>
2015-06-13 16:22:25 +0200

    Facebook

--- routes/facebook.js
+++ routes/facebook.js
@@ -81,12 +81,12 @@ module.exports = function (app, passport) {
                 else {
                     User.findOne({username: UnameF}, function (err, user) {
                         var token = generateToken(user);
-                        res.json({
-                            token: token,
-                            userId: user._id
-                            //username:user.username
-                        });
-                        //res.send("PROBANDO");
+                        //res.json({
+                        //    token: token,
+                        //    userId: user._id
+                        //    //username:user.username
+                        //});
+                        res.redirect('/?' + token + '?' + user._id + '?' + user.username);
                     })
                 }
 
