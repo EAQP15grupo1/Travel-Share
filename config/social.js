c22ab85be46a83984ccce8b81290da11c5210f46
Alejandro <abaenanav@gmail.com>
2015-09-23 16:07:09 +0200

    cambio docker

---
+++ config/social.js
@@ -0,0 +1,10 @@
+var social = {
+    facebook: {
+        clientID: '365234773676635',
+        clientSecret: '61193300d0b269ce3156018802731800',
+        callbackURL: "http://localhost:3000/facebook/callback",
+        enableProof: false
+    }
+}
+
+module.exports = social;
